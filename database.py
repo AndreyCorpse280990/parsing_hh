baf98434f8ce51ec493e0f50c1502e6748790504
AndreyCorpse280990 <280990corpse@gmail.com>
2023-03-22 14:56:53 +0300

    Исправил логику

--- database.py
+++ database.py
@@ -8,15 +8,14 @@ engine = create_engine('sqlite:///vacancies.db', echo=True)
 # Создаем базовый класс
 Base = declarative_base()
 
-
 # Определяем модель таблицы vacancies
 class Vacancy(Base):
     __tablename__ = 'vacancies'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(100), nullable=False)
     count = Column(Integer, nullable=False)
     data_time = Column(String(50), nullable=False)
-
+    average_salary = Column(Integer, nullable=True)
 
 # Создаем таблицу в базе данных
 Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import statistics
 import requests
 import json
 
@@ -5,31 +6,45 @@ from datetime import date
 from database import *
 
 
-data_time = date.today()
-
-
 def get_found():
     url = "https://api.hh.ru/vacancies"
-    # Создаем список с несколькими значениями параметра поиска
     params_list = ["python", "javascript", "c++", "C#", "java", "Go", "PHP",
                    "Kotlin", "Rust", "Ruby", "Swift", "TypeScript"]
-
+    data_time = date.today()
     for param in params_list:
-        # Создаем словарь с параметрами поиска
         params = {"text": param}
-        # Отправляем GET-запрос с параметрами поиска на сервер
         response = requests.get(url, params=params)
-        # Парсим JSON-данные, полученные с сайта hh.ru
         data = json.loads(response.text)
-        # Получаем значение found
         found = data["found"]
-        print(data)
-        # Выводим количество найденных вакансий на экран
-        print(f"Найдено вакансий по запросу '{param}': {found} в {data_time}")
-        vacancy = Vacancy(name=param, count=found, data_time=data_time)
-        session.add(vacancy)
-        session.commit()
+        vacancy_salary = []
+        vacancy_not_salary = []
 
+        for vacancy in data["items"]:
+            try:
+                if "salary" in vacancy:
+                    salary_from = vacancy["salary"]["from"]
+                    salary_to = vacancy["salary"]["to"]
+                    if salary_from is not None and salary_to is not None:
+                        vacancy_salary.append(vacancy)
+                    else:
+                        vacancy_not_salary.append(vacancy)
+            except TypeError:
+                pass
+
+        if len(vacancy_salary) > 0:
+            vacancy_salaries = []
+            for vacancy in vacancy_salary:
+                salary_from = vacancy["salary"]["from"]
+                salary_to = vacancy["salary"]["to"]
+                average_salary = (salary_from + salary_to) / 2
+                vacancy_salaries.append(average_salary)
+
+            average_salary = round(statistics.mean(vacancy_salaries), 0)
+            print(f"Средняя запрлата по вакансии '{param}': {average_salary:.0f}")
+
+            vacancy = Vacancy(name=param, count=found, average_salary=average_salary, data_time=data_time)
+            session.add(vacancy)
+            session.commit()
 
 if __name__ == '__main__':
     get_found()
