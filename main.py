2f45db6d29b88e7ea64c291613c9b3aec1908597
AndreyCorpse280990 <280990corpse@gmail.com>
2023-03-22 23:39:53 +0300

    Update main.py
    Добавил коменты

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ logging.basicConfig(filename='vacancies.log', level=logging.WARNING,
 
 def get_found(session):
     url = "https://api.hh.ru/vacancies"
+    # список вакансий, можно изменить при необходимости.
     params_list = ["python", "javascript", "c++", "C#", "java", "Go", "PHP",
                    "Kotlin", "Rust", "Ruby", "Swift", "TypeScript"]
     data_time = date.today()
     for param in params_list:
         try:
             params = {"text": param}
-            response = requests.get(url, params=params)
-            response.raise_for_status() # проверка нет ли ошибок в ответе
+            response = requests.get(url, params=params)#отправляем запрос
+            response.raise_for_status()# проверка нет ли ошибок в ответе
             data = json.loads(response.text)
             found = data["found"]
             vacancy_salary = []
@@ -31,17 +32,20 @@ def get_found(session):
             for vacancy in data["items"]:
                 try:
                     if "salary" in vacancy:
-                        salary_from = vacancy["salary"]["from"]
-                        salary_to = vacancy["salary"]["to"]
+                        salary_from = vacancy["salary"]["from"] # зп от
+                        salary_to = vacancy["salary"]["to"] # зп до
                         if salary_from is not None and salary_to is not None:
+                            # добавляем в список с зп если два значения верны
                             vacancy_salary.append(vacancy)
                         else:
+                            # Если нет, то в отдельный список
                             vacancy_not_salary.append(vacancy)
                 except TypeError:
                     logging.warning(f"Ошибка при обработке вакансии {vacancy} in {param}")
 
             if len(vacancy_salary) > 0:
                 vacancy_salaries = []
+                # высчитываем среднюю зп из vacancy_salary
                 for vacancy in vacancy_salary:
                     salary_from = vacancy["salary"]["from"]
                     salary_to = vacancy["salary"]["to"]
@@ -50,7 +54,7 @@ def get_found(session):
 
                 average_salary = round(statistics.mean(vacancy_salaries), 0)
                 print(f"Средняя зарплата по вакансии '{param}': {average_salary:.0f}")
-
+                # добавляем значения в базу данных
                 vacancy = Vacancy(name=param, count=found, average_salary=average_salary, data_time=data_time)
                 session.add(vacancy)
                 session.commit()
